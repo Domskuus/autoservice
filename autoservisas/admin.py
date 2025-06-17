c4df23c1fc62fa556b959b089545a78580197688
Domantas Zalionka <Domantaszalionka@gmail.com>
2025-06-17 08:08:52 +0300

    Adding more design

--- autoservisas/admin.py
+++ autoservisas/admin.py
@@ -2,8 +2,38 @@ from django.contrib import admin
 from .models import CarModel, Service, Car, Order, OrderLine
  # Register your models here.
 
+class carAdmin(admin.ModelAdmin):
+    list_display = ['client_name', 'auto_model', 'license_plate', 'vin_number' ]
+    list_filter = ['client_name', "auto_model"]
+    search_fields = ['license_plate', 'vin_number']
+
+class OrderInstanceLine(admin.TabularInline):
+    model = OrderLine
+    can_delete = True
+    extra = 1
+
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display =  ['car', 'date']
+    inlines = [OrderInstanceLine]
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price']
+
+class OrderLineAdmin(admin.ModelAdmin):
+    list_display = ['order', 'quantity', 'service', 'bendra_suma']
+
+    def bendra_suma(self, obj):
+        return obj.total_sum()
+    bendra_suma.short_description = 'Bendra Suma'
+
+
+
+
+
 admin.site.register(CarModel)
-admin.site.register(Service)
-admin.site.register(Car)
-admin.site.register(Order)
-admin.site.register(OrderLine)
+admin.site.register(Service, ServiceAdmin)
+admin.site.register(Car, carAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderLine, OrderLineAdmin)
--- autoservisas/models.py
+++ autoservisas/models.py
@@ -7,24 +7,38 @@ class CarModel(models.Model):
     make = models.CharField(verbose_name="Gamintojas", max_length=200, help_text="Iveskite automobilio marke")
 
     def __str__(self):
-        return f" Modelis {self.model}, marke  {self.make}"
+        return f" {self.make}, {self.model}"
+
+    class Meta:
+        verbose_name = "Automobilio Modelis"
+        verbose_name_plural = "Automobilių Modeliai"
+
+
 
 class Car(models.Model):
     license_plate = models.CharField(verbose_name="Valstybinis Numeris", max_length=7, help_text="Iveskite valstybini numeri")
-    auto_model = models.CharField(verbose_name="Modelis", max_length=200, help_text="Iveskite masinos modeli ")
+    auto_model = models.ForeignKey(to="CarModel", on_delete=models.SET_NULL, null=True, blank=True)
     vin_number = models.CharField(verbose_name="VIN Kodas", max_length=20, help_text="Iveskite VIN koda")
     client_name = models.CharField(verbose_name="Klientas", max_length=20)
-    car_model = models.ForeignKey(to="CarModel", on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
-        return f" Masinos modelis {self.car_model}, masinos numeris {self.license_plate}"
+        return f" Masinos modelis {self.auto_model}, masinos numeris {self.license_plate}"
+
+    class Meta:
+        verbose_name = "Automobilis"
+        verbose_name_plural = "Automobiliai"
+
 
 class Service(models.Model):
     name = models.CharField(verbose_name="Pavadinimas", max_length=100)
     price = models.IntegerField(verbose_name="Kaina")
 
     def __str__(self):
-        return f" Serviso pavadinimas {self.name}, kaina  {self.price}"
+        return f"{self.name},{self.price}"
+
+    class Meta:
+        verbose_name = 'Paslauga'
+        verbose_name_plural = 'Paslaugos'
 
 
 class Order(models.Model):
@@ -33,13 +47,27 @@ class Order(models.Model):
     # TODO total
 
     def __str__(self):
-        return f" Automobilis {self.car}, Data  {self.date}"
+        return f" Uzsakymas {self.id}, Data : {self.date}, Automobilis  {self.car}"
+
+    class Meta:
+        verbose_name = "Užsakymas"
+        verbose_name_plural = "Užsakymai"
+
+
 
 class OrderLine(models.Model):
     order = models.ForeignKey(to="Order",verbose_name="Uzsakymas", on_delete=models.CASCADE)
     service = models.ForeignKey(to="Service", verbose_name="Paslauga", on_delete=models.SET_NULL, null=True, blank=True)
     quantity = models.IntegerField(verbose_name="Kiekis")
-    # TODO SUM
+
+    def total_sum(self):
+        if self.service:
+            return self.quantity * self.service.price
+        return 0
 
     def __str__(self):
-        return f"{self.service} - {self.quantity}"
+        return f"{self.service} - {self.quantity}, Bendra Suma :{self.total_sum()}"
+
+    class Meta:
+        verbose_name = "Užsakymo eilutė"
+        verbose_name_plural = "Užsakymo eilutės"
